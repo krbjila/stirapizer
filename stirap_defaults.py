af12d19af7c4124ec0d77f023e4305d8312d7c76
KRbJILA <krb.jila@gmail.com>
2021-09-28 14:27:18 -0600

    latest defaults

--- stirap_defaults.py
+++ stirap_defaults.py
@@ -7,12 +7,12 @@ DOWN_ADDRESS = 10
 
 ############# Dark Resonance Parameters ##############
 UP_V_DR = 15.0
-DOWN_V_DR = 500.0
+DOWN_V_DR = 400.0
 
 
 ################## STIRAP Parameters #################
-UP_V_STIRAP = 1750.0
-DOWN_V_STIRAP = 1500.0
+UP_V_STIRAP = 1500.0
+DOWN_V_STIRAP = 900.0
 
 T_ON = 4.0
 T_STIRAP = 4.0
